da6d18af9da4c0f49d7b7749cb0aab11ea20b207
Jon Higger <jonathan.higger@gmail.com>
2025-07-07 15:56:59 -0400

    fixed warning

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { MDXVideo } from "@/components/mdx/MDXVideo";
 
 // Custom code component with syntax highlighting
