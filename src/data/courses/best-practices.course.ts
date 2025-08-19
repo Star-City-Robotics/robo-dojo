933e8c205506b14f838bfe75a823b2a5fb9ef4f8
Steven F. LeBrun <steven@lebruns.com>
2025-08-19 00:59:52 -0400

    Added a new course Best Practices for teaching the team members who are learning programming the best way to do things.  Since best practices are not Java specific, the lessons in this course were not placed in the Java Programming course.
    Two sessions are included with the initial creation of this
    course: Documenting Code and Doxygen: A Documentation Tool.

---
+++ src/data/courses/best-practices.course.ts
@@ -0,0 +1,45 @@
+// Best Practices
+//
+// A Tutorial on best programming practices that many be programming
+// language independent.
+//
+
+import DocumentingMdx from "../content/documenting-code.mdx";
+import DoxygenMdx from "../content/doxygen.mdx";
+
+import type { Course } from "../types";
+
+export const bestPracticesCourse: Course = {
+	id: "best-practices",
+	name: "Best Practices  ",
+	description: "Lessons on best practices in programming.",
+	modules: [
+		{
+			id: "best-practices",
+			name: "Best Programming Practices",
+			description: "Best Programming Practices",
+			resources: [
+				{
+					resourceType: "guide",
+					id: "documenting-code",
+					name: "Documenting Code",
+					description: "Why source code should be documented.",
+					content: DocumentingMdx,
+					completed: false,
+					icon: "book",
+					iconColor: "red",
+				},
+				{
+					resourceType: "guide",
+					id: "doxygen",
+					name: "Doxygen: A Documentation Tool",
+					description: "How to use Doxygen and install it.",
+					content: DoxygenMdx,
+					completed: false,
+					icon: "book",
+					iconColor: "purple",
+				}, // end of Doxygen Module
+			],
+		},
+	],
+};
